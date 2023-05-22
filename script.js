fa29d06f33f403d4fbc98fc0505e4490d87f7d6a
Marko V <101661316+marvuk95@users.noreply.github.com>
2023-05-22 01:52:51 -0700

    Make board squares generate dynamically

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ let output = document.getElementById("range-numbers");
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = `${this.value} X ${this.value}`;
-  
+  let container = document.getElementById("container");
+
+  for(let i=0; i < (this.value*this.value); i++) {
+    let box = document.createElement('div');
+    box.classList.add('created-box');
+    container.appendChild(box);
+  }
 }
