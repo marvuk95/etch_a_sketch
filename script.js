c60ac551f055673dcce16271e63dc4c897ebb222
Marko V <101661316+marvuk95@users.noreply.github.com>
2023-06-01 20:26:31 -0700

    Create and delete drawing board cells dynamically

--- script.js
+++ script.js
@@ -7,9 +7,22 @@ slider.oninput = function() {
   output.innerHTML = `${this.value} X ${this.value}`;
   let container = document.getElementById("container");
 
+  if (container.hasChildNodes()) {
+    while (container.hasChildNodes()) {
+      container.removeChild(container.firstChild);
+    }
+  };
+
   for(let i=0; i < (this.value*this.value); i++) {
+  
     let box = document.createElement('div');
     box.classList.add('created-box');
     container.appendChild(box);
+    let height = `${500 / this.value}px`;
+    let width =  `${500 / this.value}px`;
+    box.style.height = height;
+    box.style.width = width;
   }
+
+
 }
